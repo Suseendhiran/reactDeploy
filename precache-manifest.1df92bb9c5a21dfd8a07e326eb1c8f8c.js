91cb485d4116887afe581241d7becb12da893068
Suseendhiran <suseendhiran291996@gmail.com>
2020-05-16 23:15:49 +0530

    Updates

---
+++ precache-manifest.1df92bb9c5a21dfd8a07e326eb1c8f8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b432965e6351124766fb8848d8739e13",
+    "url": "/reactDeploy/index.html"
+  },
+  {
+    "revision": "67da074ec19a9ed7721d",
+    "url": "/reactDeploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "943d4c16c8aa0a0e0f2b",
+    "url": "/reactDeploy/static/js/2.fadd390f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactDeploy/static/js/2.fadd390f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67da074ec19a9ed7721d",
+    "url": "/reactDeploy/static/js/main.03212a76.chunk.js"
+  },
+  {
+    "revision": "3ff931e7d8b51f0bb38d",
+    "url": "/reactDeploy/static/js/runtime-main.d2ddbc69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactDeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
